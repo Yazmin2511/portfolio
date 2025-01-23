b4d539d97b0685610477d8f9feb9d2463655bad1
IvanS4L4S <yazmin2511@gmail.com>
2025-01-23 18:27:15 -0300

    Actualiza footer
    agrega iconos , crea estilos para footer

--- portfolio/app/components/footer.tsx
+++ portfolio/app/components/footer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import '../css/footer.css'; 
 
-const footer = () => {
+const Footer: React.FC = () => {
   return (
-    <div>footer</div>
-  )
+    <footer>
+      <div className="footer-content">
+        <p>© 2025 Yazmin. Todos los derechos reservados.</p>
+        <div className="social-icons">
+          <a href="https://www.linkedin.com/in/tu-perfil" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/tu-usuario" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://twitter.com/tu-usuario" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default footer
+export default Footer;
--- portfolio/app/components/header.tsx
+++ portfolio/app/components/header.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import '../css/header.css';
 import { typeWriter } from '~/utils/typeWriter';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import {faStar,faFolderOpen, faMessage } from '@fortawesome/free-solid-svg-icons';
 
 const header: React.FC = () => {
   useEffect(() => { 
@@ -11,9 +13,9 @@ const header: React.FC = () => {
       
       <h1 id="typed-name" className='gradient-text'></h1> 
       <nav> 
-        <a href="#about">Sobre Mí</a> 
-        <a href="#projects">Proyectos</a> 
-        <a href="#contact">Contacto</a> 
+        <a href="#about"><FontAwesomeIcon icon={faStar} /> Sobre Mí</a> 
+        <a href="#projects"><FontAwesomeIcon icon={faFolderOpen} /> Proyectos</a> 
+        <a href="#contact"><FontAwesomeIcon icon={faMessage} /> Contacto</a> 
       </nav> 
     </header>
   );
